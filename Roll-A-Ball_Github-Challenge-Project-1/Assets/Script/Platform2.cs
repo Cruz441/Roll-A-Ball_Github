ab779894e5cde474beaf7a6d5930d6ab556670c7
Cruz441 <Cruz441@users.noreply.github.com>
2018-09-22 20:27:05 -0400

    Challenge1
    Adding new colors, moving wall, new pay field, removing points

---
+++ Roll-A-Ball_Github-Challenge-Project-1/Assets/Script/Platform2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform2 : MonoBehaviour {
+
+    private Vector3 pos1;
+
+    private Vector3 pos2;
+
+    private Vector3 nexPos;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Transform childTransform;
+
+    [SerializeField]
+    private Transform transform2;
+
+	// Use this for initialization
+	void Start () {
+        pos1 = childTransform.localPosition;
+        pos2 = transform2.localPosition;
+        nexPos = pos2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nexPos, speed * Time.deltaTime);
+
+        if(Vector3.Distance(childTransform.localPosition,nexPos) <= 0.1)
+        {
+            ChangeDestination();
+        }
+    }
+    private void ChangeDestination()
+    {
+        nexPos = nexPos != pos1 ? pos1 : pos2;
+    }
+}
---
+++ Roll-A-Ball_Github-Challenge-Project-1/Assets/Script/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text yellowpText;
+    
+
+    private Rigidbody rb;
+    private int count;
+    private int yellowp;
+    private object Player;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        yellowp  = 0;
+        SetYellowText();
+        winText.text = "";
+       
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText ();
+            yellowp = yellowp + 1;
+            SetYellowText();
+        }
+
+        if (other.gameObject.CompareTag("EvilCube"))
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            SetCountText();
+        }
+
+        if (count == 12)
+        {
+            transform.position = new Vector3(22.38f, 0.5f, 7.13f);
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You won with a score of: " + count.ToString();
+        }
+    }
+
+    void SetYellowText()
+    {
+        yellowpText.text = "Pickups: " + yellowp.ToString();
+      
+    }
+}
